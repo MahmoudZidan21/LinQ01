7add8a0aba4b88385dc6a73223044bd6bad2aa6b
NanoChip <NanoChip@DESKTOP-2104TCA>
2025-02-08 11:30:47 +0200

    Implicitly Typed Local Variables (var - Dynamic)

--- LinQ01/Program.cs
+++ LinQ01/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Implicitly Typed Local Variables (var - Dynamic)
+
+            // Implicitly Typed Local Variables (var - Dynamic)
+
+            // var
+            //   var  Data01 = " Ahmed";
+            // Copmiler  can Detect the Datatype of the Variable
+            // Must Be Initialized
+            // Can't Be Null
+            // Can't  Change THE DataType  
+            // Can't Be Used as A prameter or Return Type
+
+
+            //  Data01 = 12; // Invalid 
+            // =======================================
+
+            // Dynamic 
+            //// Like var in JS
+
+            //dynamic Data02 = "Ahmed";
+            //// CLR Detect the Datatype of the Variable Based On The Last Value at RunTime
+            //// don't need to be Initialized
+            //// Can Be Null
+            //// Can Change The DataType After Initialization
+            //// Can Be Used as A prameter or Return Type
+
+
+
+
+            //Data02 = 12;
+            //Console.WriteLine(Data02.GetType().Name);
+            //Data02 = 12.5;
+            //Console.WriteLine(Data02.GetType().Name);
+            //Data02 = true;
+            //Console.WriteLine(Data02.GetType().Name); 
+            #endregion
+
+
+
         }
     }
 }
